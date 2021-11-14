b306493f75ea4bd35c2589610a5042504f267979
Salmaan <salmaan.arif.ali@gmail.com>
2021-11-14 01:10:35 -0500

    add focus css

--- src/containers/calculator.js
+++ src/containers/calculator.js
@@ -21,31 +21,31 @@ class Calculator extends React.Component {
         <table id="calculator" onClick={(e) => this.handleClick(e)}>
           <tr>
             <td id="result" colSpan="3">{this.state.currentNumber}</td>
-            <td id="clear" className="operator">AC</td>
+            <td id="clear" className="operator button">AC</td>
           </tr>
           <tr>
-            <td id="1" className="number">1</td>
-            <td id="2" className="number">2</td>
-            <td id="3" className="number">3</td>
-            <td id="divide" className="operator">/</td>
+            <td id="1" className="number button">1</td>
+            <td id="2" className="number button">2</td>
+            <td id="3" className="number button">3</td>
+            <td id="divide" className="operator button">/</td>
           </tr>
           <tr>
-            <td className="number">4</td>
-            <td className="number">5</td>
-            <td className="number">6</td>
-            <td id="multiply" className="operator">x</td>
+            <td className="number button">4</td>
+            <td className="number button">5</td>
+            <td className="number button">6</td>
+            <td id="multiply" className="operator button">x</td>
           </tr>
           <tr>
-            <td className="number">7</td>
-            <td className="number">8</td>
-            <td className="number">9</td>
-            <td id="subtract" className="operator">-</td>
+            <td className="number button">7</td>
+            <td className="number button">8</td>
+            <td className="number button">9</td>
+            <td id="subtract" className="operator button">-</td>
           </tr>
           <tr>
-            <td className="number">0</td>
-            <td className="number">.</td>
+            <td className="number button">0</td>
+            <td className="number button">.</td>
             <td id="equal">=</td>
-            <td id="add" className="operator">+</td>
+            <td id="add" className="operator button">+</td>
           </tr>
         </table>
       </div>
