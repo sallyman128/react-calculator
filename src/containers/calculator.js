78cf4480f661c11d2dfade12d8f6c2f062b2f539
Salmaan <salmaan.arif.ali@gmail.com>
2021-11-14 15:47:26 -0500

    allow user to type their input

--- src/containers/calculator.js
+++ src/containers/calculator.js
@@ -14,19 +14,30 @@ class Calculator extends React.Component {
   handleClick = (e) => {
     const eventClasses = e.target.classList
     const eventID = e.target.id
-    if (eventClasses.contains("number")) {
+    if (eventClasses.contains("calculator-number")) {
       this.setState(prevState => ({
         currentNumber: prevState.currentNumber + e.target.innerText
       }))
-    } else if (eventClasses.contains("operator")) {
+    } else if (eventClasses.contains("calculator-operator")) {
       this.setState(prevState => ({
         firstNumber: prevState.currentNumber,
         currentNumber: ""
       }))
-    } else if (eventID === "clear") {
+    } else if (eventID === "calculator-clear") {
       this.setState({
         currentNumber: ""
       })
+    }
+  }
+
+  handleKeyUp = (e) => {
+    const isNumber = Number.isInteger(parseFloat(e.key));
+    const isBackspace = e.key === "Backspace";
+
+    if (isNumber) {
+      this.setState(prevState => ({
+        currentNumber: prevState.currentNumber + e.key
+      }))
     }
   }
 
@@ -35,32 +46,34 @@ class Calculator extends React.Component {
       <div>
         <table id="calculator" onClick={(e) => this.handleClick(e)}>
           <tr>
-            <td id="result" colSpan="3">{this.state.currentNumber}</td>
-            <td id="clear" className="button">AC</td>
+            <td value="result" id="calculator-result" colSpan="3">
+              <input onKeyUp={(e) => this.handleKeyUp(e)} type='text' value={this.state.currentNumber} />
+            </td>
+            <td value="clear" id="calculator-clear">AC</td>
           </tr>
           <tr>
-            <td id="1" className="number button">1</td>
-            <td id="2" className="number button">2</td>
-            <td id="3" className="number button">3</td>
-            <td id="divide" className="operator button">/</td>
+            <td type="button" value="1" className="calculator-number">1</td>
+            <td type="button" value="2" className="calculator-number">2</td>
+            <td type="button" value="3" className="calculator-number">3</td>
+            <td type="button" value="/" className="calculator-operator">/</td>
           </tr>
           <tr>
-            <td className="number button">4</td>
-            <td className="number button">5</td>
-            <td className="number button">6</td>
-            <td id="multiply" className="operator button">x</td>
+            <td type="button" value="4" className="calculator-number">4</td>
+            <td type="button" value="5" className="calculator-number">5</td>
+            <td type="button" value="6" className="calculator-number">6</td>
+            <td type="button" value="x" className="calculator-operator">x</td>
           </tr>
           <tr>
-            <td className="number button">7</td>
-            <td className="number button">8</td>
-            <td className="number button">9</td>
-            <td id="subtract" className="operator button">-</td>
+            <td type="button" value="7" className="calculator-number">7</td>
+            <td type="button" value="8" className="calculator-number">8</td>
+            <td type="button" value="9" className="calculator-number">9</td>
+            <td type="button" value="-" className="calculator-operator">-</td>
           </tr>
           <tr>
-            <td className="number button">0</td>
-            <td className="number button">.</td>
-            <td id="equal">=</td>
-            <td id="add" className="operator button">+</td>
+            <td type="button" value="0" className="calculator-number">0</td>
+            <td type="button" value="." className="calculator-number">.</td>
+            <td type="button" value="=" id="calculator-equal">=</td>
+            <td type="button" value="+" className="calculator-operator">+</td>
           </tr>
         </table>
       </div>
